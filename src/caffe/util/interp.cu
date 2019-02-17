a102bfcddb77284376edf9303bad9e490969b724
Sangwon Lee <leee.sangwon@gmail.com>
2019-02-17 22:45:55 +0900

    Edit InterpLayer
    caffe_gpu_atomic_add

--- src/caffe/util/interp.cu
+++ src/caffe/util/interp.cu
@@ -149,10 +149,10 @@ __global__ void caffe_gpu_interp2_kernel_backward(const int n, const float rheig
       Dtype* pos1 = &data1[channels * ((y1 + h1) * Width1 + (x1 + w1))];
       const Dtype* pos2 = &data2[channels * ((y2 + h2) * Width2 + (x2 + w2))];
       for (int c = 0; c < channels; ++c) {
-	caffe_gpu_atomic_add<Dtype>(&pos1[0], h0lambda * w0lambda * pos2[0]);
-	caffe_gpu_atomic_add<Dtype>(&pos1[channels * w1p], h0lambda * w1lambda * pos2[0]);
-	caffe_gpu_atomic_add<Dtype>(&pos1[channels * h1p * Width1], h1lambda * w0lambda * pos2[0]);
-	caffe_gpu_atomic_add<Dtype>(&pos1[channels * (h1p * Width1 + w1p)], h1lambda * w1lambda * pos2[0]);
+	caffe_gpu_atomic_add(&pos1[0], h0lambda * w0lambda * pos2[0]);
+	caffe_gpu_atomic_add(&pos1[channels * w1p], h0lambda * w1lambda * pos2[0]);
+	caffe_gpu_atomic_add(&pos1[channels * h1p * Width1], h1lambda * w0lambda * pos2[0]);
+	caffe_gpu_atomic_add(&pos1[channels * (h1p * Width1 + w1p)], h1lambda * w1lambda * pos2[0]);
 	pos1++;
 	pos2++;
       }
@@ -161,10 +161,10 @@ __global__ void caffe_gpu_interp2_kernel_backward(const int n, const float rheig
       Dtype* pos1 = &data1[(y1 + h1) * Width1 + (x1 + w1)];
       const Dtype* pos2 = &data2[(y2 + h2) * Width2 + (x2 + w2)];
       for (int c = 0; c < channels; ++c) {
-	caffe_gpu_atomic_add<Dtype>(&pos1[0], h0lambda * w0lambda * pos2[0]);
-	caffe_gpu_atomic_add<Dtype>(&pos1[w1p], h0lambda * w1lambda * pos2[0]);
-	caffe_gpu_atomic_add<Dtype>(&pos1[h1p * Width1], h1lambda * w0lambda * pos2[0]);
-	caffe_gpu_atomic_add<Dtype>(&pos1[h1p * Width1 + w1p], h1lambda * w1lambda * pos2[0]);
+	caffe_gpu_atomic_add(&pos1[0], h0lambda * w0lambda * pos2[0]);
+	caffe_gpu_atomic_add(&pos1[w1p], h0lambda * w1lambda * pos2[0]);
+	caffe_gpu_atomic_add(&pos1[h1p * Width1], h1lambda * w0lambda * pos2[0]);
+	caffe_gpu_atomic_add(&pos1[h1p * Width1 + w1p], h1lambda * w1lambda * pos2[0]);
 	pos1 += Width1 * Height1;
 	pos2 += Width2 * Height2;
       }
